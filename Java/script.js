e46a1b6e423c2412d31f0eaed504d68a1c94680b
Santinho <santiagob7867@gmail.com>
2023-10-08 15:22:14 -0500

    duodécimo o decimotercer commit, terminé el index. Más que todo me enfoqué en los servicios. Ahora puedo enfocarme de lleno en los formularios.

--- Java/script.js
+++ Java/script.js
@@ -2,3 +2,14 @@ window.onload = function(){
     $('#onload').fadeOut();
     $('body').removeClass('hidden');
 }
+
+document.addEventListener('DOMContentLoaded', function() {
+    let hideInfoBtns = document.querySelectorAll('.card');
+    let hideInfoElements = document.querySelectorAll('.card_info');
+
+    hideInfoBtns.forEach((button, index) => {
+        button.addEventListener('click', function() {
+            hideInfoElements[index].classList.toggle('show');
+        });
+    });
+});
