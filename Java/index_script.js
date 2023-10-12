c5b53a88882e2e44f950d882cb80c8135efc2d91
Santinho <santiagob7867@gmail.com>
2023-10-12 12:47:00 -0500

    Decimoquinto commit, todos los requisitos del proyecto para la ruta básica están en la página. Falta organizar mejor el código. Y de manera opcional agregar cosas extras.

--- Java/index_script.js
+++ Java/index_script.js
@@ -1,26 +1,38 @@
-window.onload = function(){
-    $('#onload').fadeOut();
-    $('body').removeClass('hidden');
-}
+window.onload = function () {
+  $("#onload").fadeOut();
+  $("body").removeClass("hidden");
+};
 
-document.addEventListener('DOMContentLoaded', function() {
-    let hideInfoBtns = document.querySelectorAll('.card');
-    let hideInfoElements = document.querySelectorAll('.card_info');
+document.addEventListener("DOMContentLoaded", function () {
+  let hideInfoBtns = document.querySelectorAll(".card");
+  let hideInfoElements = document.querySelectorAll(".card_info");
 
-    hideInfoBtns.forEach((button, index) => {
-        button.addEventListener('click', function() {
-            hideInfoElements[index].classList.toggle('show');
-        });
+  hideInfoBtns.forEach((button, index) => {
+    button.addEventListener("click", function () {
+      hideInfoElements[index].classList.toggle("show");
     });
+  });
+
+  const user = JSON.parse(localStorage.getItem("login_sucess")) || false;
+  const user_name = document.getElementById("user_name");
+
+  const logout = document.querySelector("#logout");
+  logout.addEventListener("click", () => {
+    alert("Sesión cerrada correctamente"),
+      localStorage.removeItem("login_sucess");
+    this.location.reload();
+  });
+
+  function prevenirRedireccion(event) {
+    event.preventDefault();
+  }
+
+  if (user) {
+    document.getElementById("user_name").innerText = user.name;
+    logout.style.display = "block";
+    user_name.style.cursor = "default";
+    user_name.addEventListener("click", prevenirRedireccion);
+  } else {
+    logout.style.display = "none";
+  }
 });
-
-
-document.addEventListener('DOMContentLoaded', function() {
-
-const user = JSON.parse(localStorage.getItem('login_sucess')) || false
-
-const logout = document.querySelector('#logout')
-logout.addEventListener('click', ()=>{
-    return alert('Sesión cerrada correctamente')
-    localStorage.removeItem('login_sucess')
-})})
--- Java/register_script.js
+++ Java/register_script.js
@@ -3,13 +3,16 @@ window.onload = function(){
     $('body').removeClass('hidden');
 }
 
-const signupForm = document.querySelector('#signup_form')
-signupForm.addEventListener('submit', (e)=>{
+const signup_form = document.querySelector('#signup_form')
+signup_form.addEventListener('submit', (e)=>{
     e.preventDefault()
-    const name = document.querySelector('#name').value
-    const second_name = document.querySelector('#second_name')
-    const email = document.querySelector('#email').value
-    const password = document.querySelector('#password').value
+    const name = document.querySelector('#name').value;
+    const second_name = document.querySelector('#second_name').value;
+    const email = document.querySelector('#email').value;
+    const password = document.querySelector('#password').value;
+    const man = document.getElementById('man_sex').checked;
+    const woman = document.getElementById('woman_sex').checked;
+
 
     const Users = JSON.parse(localStorage.getItem('users')) || []
     const isUserRegistered = Users.find(user => user.email === email)
@@ -17,7 +20,9 @@ signupForm.addEventListener('submit', (e)=>{
         return alert('EL usuario registrado ya existe')
     }
 
-    Users.push({name: name, second_name: second_name, email: email, password: password})
+    const sex = man ? 'Hombre' : (woman ? 'Mujer' : '');
+
+    Users.push({name: name, second_name: second_name, email: email, password: password, sex: sex})
     localStorage.setItem('users', JSON.stringify(Users))
     return alert('Te has registrado con éxito'),
     window.location.href = 'Login.html'
