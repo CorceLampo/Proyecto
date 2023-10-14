b8f4c59ecc5e7c0eab6ef4e4d332504c9cb23aff
Santinho <santiagob7867@gmail.com>
2023-10-14 18:11:14 -0500

    Decimosexto commit, decidí hacer un carro de compras funcional; adelanté mucha lógica con JS. Toda la información del producto se guarda en el Local Storage, falta implementarla en una tabla (que sería el carŕo de compras en sí).

--- Java/index_script.js
+++ Java/index_script.js
@@ -35,4 +35,123 @@ document.addEventListener("DOMContentLoaded", function () {
   } else {
     logout.style.display = "none";
   }
+
+  let products = [
+    {
+      name: "Alegro ma non troppo",
+      tag: "Book-1",
+      price: "30000",
+      inCart: "0",
+    },
+    {
+      name: "Adam Smith",
+      tag: "Book-2",
+      price: "110000",
+      inCart: "0",
+    },
+    {
+      name: "La Regenta",
+      tag: "Book-3",
+      price: "20000",
+      inCart: "0",
+    },
+    {
+      name: "Tragedias",
+      tag: "Book-4",
+      price: "30000",
+      inCart: "0",
+    },
+    {
+      name: "Meditaciones",
+      tag: "Book-5",
+      price: "30000",
+      inCart: "0",
+    },
+    {
+      name: "Fahrenheit 451",
+      tag: "Book-6",
+      price: "30000",
+      inCart: "0",
+    },
+    {
+      name: "Santa Biblia",
+      tag: "Book-7",
+      price: "20000",
+      inCart: "0",
+    },
+    {
+      name: "El Corán",
+      tag: "Book-8",
+      price: "30000",
+      inCart: "0",
+    },
+    {
+      name: "Historia Mínima de Colombia",
+      tag: "Book-9",
+      price: "30000",
+      inCart: "0",
+    },
+    {
+      name: "El Hombre en busca de Sentido",
+      tag: "Book-10",
+      price: "30000",
+      inCart: "0",
+    },
+  ];
+
+  let carts = document.querySelectorAll(".add_cart");
+  for (let i = 0; i < carts.length; i++) {
+    carts[i].addEventListener("click", () => {
+      cartNumbers(products[i]);
+      totalCost(products[i]);
+    });
+  }
+
+  function cartNumbers(product) {
+    let productNumbers = localStorage.getItem("cartNumbers");
+    productNumbers = parseInt(productNumbers);
+    if (productNumbers) {
+      localStorage.setItem("cartNumbers", productNumbers + 1);
+    } else {
+      localStorage.setItem("cartNumbers", 1);
+    }
+    setItems(product);
+  }
+
+  function setItems(product) {
+    let cartItems = localStorage.getItem("productsInCart");
+    cartItems = JSON.parse(cartItems);
+
+    if (cartItems != null) {
+      if (cartItems[product.tag] == undefined) {
+        cartItems = {
+          ...cartItems,
+          [product.tag]: product,
+        };
+      }
+      product.inCart = 0;
+      cartItems[product.tag].inCart += 1;
+
+    } else {
+      product.inCart = 1;
+      
+      cartItems = {
+        [product.tag]: product,
+      };
+    }
+
+    console.log("My cart items are", cartItems);
+    localStorage.setItem("productsInCart", JSON.stringify(cartItems));
+  }
+
+  function totalCost(product) {
+    let cartCost = localStorage.getItem("totalCost");
+    if (cartCost != null) {
+      cartCost = parseInt(cartCost);
+      product.price = parseInt(product.price)
+      localStorage.setItem("totalCost", cartCost + product.price);
+    } else {
+      localStorage.setItem("totalCost", product.price);
+    }
+  }
 });
